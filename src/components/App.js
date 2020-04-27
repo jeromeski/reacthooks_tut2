a3810c40b910d3ae5060494553aaa2d7b2a732ac
Jeromeski <jeromegee@gmail.com>
2020-04-27 20:09:21 +0800

    Code reuse with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 function App() {
-  const [ useResource, setResource ] = useState('posts')
+  const [useResource, setResource] = useState('posts');
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
       </div>
-      <ResourceList resource={useResource}/>
+      <ResourceList resource={useResource} />
     </div>
   );
 }
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,24 +1,7 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import userResources from './userResources'
 
 
-const userResources = resource => {
-  const [resources, setResources] = useState([]);
-
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResources(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
-
-  return resources
-};
 
 const ResourceList = ({ resource }) => {
   const resources = userResources(resource);
