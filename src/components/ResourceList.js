4a8491063597eb5c2075fe31aeb92768dcdb4bf4
Jeromeski <jeromegee@gmail.com>
2020-04-27 19:07:07 +0800

    useEffect using async IIF

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
@@ -26,3 +27,27 @@ const ResourceList = ({ resource }) => {
 export default ResourceList;
 
 // {resources.map(({id, title}) => <p key={id}>{title}</p>)}
+
+/*
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    (async resource => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(response.data);
+  })(resource);
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resource]);
+  return (
+    <div>
+      <p>{resources.length}</p>
+    </div>
+  );
+};
+
+export default ResourceList;
+
+*/
