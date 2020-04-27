5408c49288cd6c9ab96bc5f387d43b25d0162a79
Jeromeski <jeromegee@gmail.com>
2020-04-27 19:41:18 +0800

    Actual code reuse

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,11 +1,12 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-const ResourceList = ({ resource }) => {
+const userResources = resource => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async (resource) => {
+  const fetchResource = async resource => {
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/${resource}`
     );
@@ -14,13 +15,20 @@ const ResourceList = ({ resource }) => {
 
   useEffect(() => {
     fetchResource(resource);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [resource]);
 
+  return resources
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = userResources(resource);
+
   return (
-
-      <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
-
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
   );
 };
 
